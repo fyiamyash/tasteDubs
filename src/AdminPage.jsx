0db289705d0e6de7b25692953e12b4a7dbfc108f
fyiamyash <yashmeshram786@gmail.com>
2024-02-01 02:19:43 +0530

    displying menu items

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -3,6 +3,7 @@ import TdAppbar from "./TdAppbar";
 import TabComponent from "./TabComponent";
 import { useRecoilValue } from "recoil";
 import { tabValue } from "./store/atoms/tabValue";
+import { Typography } from "@mui/material";
 
 function AdminPage() {
     const [menu, setMenu] = useState([]);
@@ -22,22 +23,22 @@ function AdminPage() {
                 const result = await response.json();
                 if (result) {
                     setMenu(result.menu);
-                    let burgerArray=[];
-                    let wrapArray=[];   
-                    let sliderArray=[];   
-            result.menu.map((item)=>{
-                if (item.title =="burger") {
-                  {burgerArray.push(item)}
-                }else if(item.title=="wrap"){
-                    {wrapArray.push(item)}
-                }
-                else{
-                    {sliderArray.push(item)}
-                }
-            })
-            setBurger(burgerArray);
-            setWrap(wrapArray);
-            setSlider(sliderArray);
+                    let burgerArray = [];
+                    let wrapArray = [];
+                    let sliderArray = [];
+                    result.menu.map((item) => {
+                        if (item.title == "burger") {
+                            { burgerArray.push(item) }
+                        } else if (item.title == "wrap") {
+                            { wrapArray.push(item) }
+                        }
+                        else {
+                            { sliderArray.push(item) }
+                        }
+                    })
+                    setBurger(burgerArray);
+                    setWrap(wrapArray);
+                    setSlider(sliderArray);
                 }
 
             } catch (error) {
@@ -50,9 +51,10 @@ function AdminPage() {
     return <>
         <TdAppbar />
         <TabComponent />
-        {tabValueDiv ===0 ?(<DisplayBuWr burger = {burger} />)
-        :tabValueDiv ===1 ?(<DisplayBuWr burger ={wrap} />)
-        :(<DisplayBuWr burger ={slider} />)}
+        {tabValueDiv === 0 ? (<ItemCard burger={burger} />)
+            : tabValueDiv === 1 ? (<ItemCard burger={wrap} />)
+                : (<ItemCard burger={slider} />)}
+        {/* <RectangleCard /> */}
 
     </>
 }
@@ -73,22 +75,42 @@ function DisplayMenu(props) {
         </div>
     );
 }
-function DisplayBuWr(props)
-{
-    return<div style={{
-        marginLeft: "50px",
-        marginTop: "50px"
+function ItemCard(props) {
+    return (<div style={{
+        width: "90%",
+        height: "500px",
+        marginLeft: "70px",
+        marginTop: "20px",
+        backgroundColor: "grey",
+        padding: "20px",
+        display: "flex",
+        flexWrap: "nowrap",
+        gap:"20px", 
+        justifyContent: "flex-start",
+        overflowY: "scroll"
     }}>
-        {props.burger.map((item, index) => (
-            <div key={index}>
-                <h2>{item.title}</h2><br />
-                <h3>{item.description}</h3><br />
-                <h3>{item.price}</h3><br /> <br />
-            </div>
+        {props.burger.map(item => (
+            <RectangleCard value={item} />
         ))}
-    </div>
+    </div>);
 }
-
+function RectangleCard(props) {
+    return (
+        <div style={{
+            width: '300px',
+            height: '50px',
+            maxHeight: '500px',
+            backgroundColor: 'lightblue',
+            borderRadius: '10px',
+            padding: '10px',
+            marginBottom: '20px',
+            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        }}>
+            <h3>{props.value.description}</h3>
+            <p>Rs.{props.value.price}</p>
+        </div>
+    );
+}
 
 
 export default AdminPage;
